7e445c8e068af490ead7e730ade127dc5f802d12
Chendong HUA <waasandung@gmail.com>
2025-03-01 10:29:20 +0800

    Third commit

--- chatbot.py
+++ chatbot.py
@@ -1,28 +1,56 @@
 ## this file is based on version 13.7 of python telegram chatbot
 ## and version 1.26.18 of urllib3
 ## chatbot.py
-import telegram
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          CallbackContext)
 # The messageHandler is used for all message updates
+from ChatGPT_HKBU import HKBU_ChatGPT
 import configparser
 import logging
+import redis
 
+global redis1
 def main():
     # Load token and create Updater
     config = configparser.ConfigParser()
     config.read('config.ini')
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True )
     dispatcher = updater.dispatcher
+    global redis1
+    redis1 = redis.Redis(host=(config['REDIS']['HOST']),
+                         password=(config['REDIS']['PASSWORD']),
+                         port=(config['REDIS']['REDISPORT']),
+                         decode_responses=(config['REDIS']['DECODE_RESPONSE']),
+                         username=(config['REDIS']['USER_NAME']))
     # Set the logging module to know when and why things don't work as expected
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
     # register a dispatcher to handle message:
     # register an echo dispatcher
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
+    # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    # dispatcher.add_handler(echo_handler)
+
+    # dispatcher for chatgpt
+    global chatgpt
+    chatgpt = HKBU_ChatGPT(config)
+    chatgpt_handler = MessageHandler(Filters.text & (~Filters.command),
+                                     equiped_chatgpt)
+    dispatcher.add_handler(chatgpt_handler)
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("add", add))
+    dispatcher.add_handler(CommandHandler("help", help_command))
     # To start:
     updater.start_polling()
     updater.idle()
+
+def equiped_chatgpt(update, context):
+    global chatgpt
+    reply_message = chatgpt.submit(update.message.text)
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
 
 def echo(update, context):
     reply_message = update.message.text.upper()
@@ -30,5 +58,25 @@ def echo(update, context):
     logging.info("context: " + str(context))
     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
 
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Helping you helping you.')
+
+def add(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /add is issued."""
+    try:
+        global redis1
+        logging.info(context.args[0])
+        msg = context.args[0] # /add keyword <-- this should store the keyword
+        redis1.incr(msg)
+
+        update.message.reply_text('You have said ' + msg + ' for ' +
+                                  redis1.get(msg).decode('UTF-8') + ' times.')
+        
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /add <keyword>')
+        
 if __name__ == '__main__':
     main()
